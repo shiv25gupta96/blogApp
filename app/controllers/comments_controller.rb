1908c110651fff318ce01c68d9dd54f76038b9e2
shiv25gupta96 <shivam25gupta96@gmail.com>
2018-02-05 12:42:54 +0530

    Complete Blog App

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+
+  before_action :set_comments, only: [:show]
+
+  def create
+    @article =  Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_attribute)
+    if @comment.save
+      redirect_to article_path(params[:article_id])
+    else
+      render 'comments'
+    end
+  end
+
+  private
+
+    def comment_attribute
+      params.require('comment').permit(:user, :body)
+    end
+
 end
